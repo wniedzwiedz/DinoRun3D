b84ee62872724f0feb8d2a399c14630223f715ac
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-01-17 23:14:43 +0100

    fix timer

--- project/logic.js
+++ project/logic.js
@@ -3,30 +3,31 @@ function finishReached(player, collider) {
   document.getElementById("timer").innerHTML = "YOU SURVIVED";
   var scene = document.getElementById('game');
   scene.innerHTML = ""
-  scene.innerHTML ="<img src='https://c.tenor.com/3uCfvzgTIFUAAAAC/dinos-dinosaurs.gif'></img>";
+  scene.innerHTML = "<img src='https://c.tenor.com/3uCfvzgTIFUAAAAC/dinos-dinosaurs.gif'></img>";
 }
 
 
-var gametime = 0.5; 
-var countDownDate = new Date().getTime()+(gametime*60*1000);
+var gametime = 0.5;
+var countDownDate = new Date().getTime() + (gametime * 60 * 1000);
 
-var x = setInterval(function() {
+var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+  if (!document.getElementById("timer").innerHTML.includes("YOU")) {
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+  }
   if (distance <= 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "YOU DIED";
     var scene = document.getElementById('game');
-    scene.innerHTML="";
+    scene.innerHTML = "";
   }
 }, 1000);
 
-function updateAsteroid()
-{
+function updateAsteroid() {
   progress = 1.0 - (countDownDate / 1000 - new Date().getTime() / 1000) / (gametime * 60)
   const START = glMatrix.vec3.fromValues(-100, 100, -100)
 
@@ -47,7 +48,7 @@ function updateAsteroid()
   asteroidVec = glMatrix.vec3.create()
   glMatrix.vec3.multiply(asteroidVec, asteroidDir, glMatrix.vec3.fromValues(progress, progress, progress))
   glMatrix.vec3.add(newAsteroidPos, asteroidPos, asteroidVec)
-  
+
   if (!isNaN(newAsteroidPos[0]))
     asteroidTransform.translation = [newAsteroidPos[0], newAsteroidPos[1], newAsteroidPos[2]].join(" ")
 
