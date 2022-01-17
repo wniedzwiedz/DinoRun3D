8ecc46bac02acac16ffc1fc69cac4c641e3b9945
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-01-17 23:12:13 +0100

    fix timer

--- project/logic.js
+++ project/logic.js
@@ -17,10 +17,10 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-  if (distance < 0) {
+  if (distance <= 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "YOU DIED";
-    var scene = document.getElementById('timer');
+    var scene = document.getElementById('game');
     scene.innerHTML="";
   }
 }, 1000);
