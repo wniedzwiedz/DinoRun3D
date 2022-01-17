8fa6ff928a4c14c1b561d86ef8eccffdfec99940
Kacper Zybała <zyperpl@gmail.com>
2022-01-17 22:50:30 +0100

    Fix timer

--- project/logic.js
+++ project/logic.js
@@ -1,6 +1,6 @@
 function finishReached(player, collider) {
   console.log("YOU WON!");
-  document.getElementById("demo").innerHTML = "YOU SURVIVED";
+  document.getElementById("timer").innerHTML = "YOU SURVIVED";
   var scene = document.getElementById('game');
   scene.innerHTML = ""
   scene.innerHTML ="<img src='https://c.tenor.com/3uCfvzgTIFUAAAAC/dinos-dinosaurs.gif'></img>";
