d7df8a670a3a5db8156a61df3fe0fccf9b0f7a60
wniedzwiedz <wmniedzwiedz@gmail.com>
2022-01-17 21:49:27 +0100

    use finish callback

--- project/logic.js
+++ project/logic.js
@@ -1,4 +1,7 @@
-function finishReached(player, collider)
-{
-  console.log("YOU WON!")
+function finishReached(player, collider) {
+  console.log("YOU WON!");
+  document.getElementById("demo").innerHTML = "YOU SURVIVED";
+  var scene = document.getElementById('game');
+  scene.innerHTML = ""
+  scene.innerHTML ="<img src='https://c.tenor.com/3uCfvzgTIFUAAAAC/dinos-dinosaurs.gif'></img>";
 }
